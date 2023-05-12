bbf2a858a0ca233e07ddd941921f9a5fb57a6cbc
Alkarkhi27 <125407087+Alkarkhi27@users.noreply.github.com>
2023-05-12 01:07:30 -0400

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,23 @@
+import socket
+
+target = "127.0.0.1"
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+#print(portscan(98))
+for port in range(1, 1024):
+    result = portscan(port)
+
+    if result:
+        print("port{} is open!".format(port))
+
+    else:
+
+        print("port{} is closed!".format(port))
+
+
